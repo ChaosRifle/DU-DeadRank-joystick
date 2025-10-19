e41bc42184e2f13619f29dcfd72a3f8de9a544f0
Chaos <DC3120@gmail.com>
2025-10-19 05:26:32 -0600

    Update system.onFlush.lua with joystick support

--- DeadRemote-min/lua/system.onFlush.lua
+++ DeadRemote-min/lua/system.onFlush.lua
@@ -1,1 +1,9 @@
+rollInput = system.getAxisValue(0)
+pitchInput = system.getAxisValue(1)
+yawInput = -1 * system.getAxisValue(2)
+
+Nav.axisCommandManager:setThrottleCommand(axisCommandId.longitudinal, system.getAxisValue(3))
+Nav.axisCommandManager:setThrottleCommand(axisCommandId.lateral, system.getAxisValue(4))
+Nav.axisCommandManager:setThrottleCommand(axisCommandId.vertical, system.getAxisValue(5))  
+
 profile(runFlush,'runFlush')
