b8be710235b8b83b4adcbc4a2e0ea6348b6262b2
Chaos <DC3120@gmail.com>
2025-11-15 14:20:28 -0700

    Update system.onFlush.lua removed debug

--- DeadRemote-min/lua/system.onFlush.lua
+++ DeadRemote-min/lua/system.onFlush.lua
@@ -1,5 +1,5 @@
 if PLAYER_ID ~= 11654 then
-  system.print('not graaf')
+  --system.print('not graaf')
   rollInput = system.getAxisValue(0)
   pitchInput = system.getAxisValue(1)
   yawInput = -1 * system.getAxisValue(2)
@@ -8,7 +8,7 @@ if PLAYER_ID ~= 11654 then
   Nav.axisCommandManager:setThrottleCommand(axisCommandId.lateral, system.getAxisValue(4))
   Nav.axisCommandManager:setThrottleCommand(axisCommandId.vertical, system.getAxisValue(5))
 else
-    system.print('graaf detected')
+    --system.print('graaf detected')
 end
 
 profile(runFlush,'runFlush')
