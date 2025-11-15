d454f377babbe1351386b303389e6b7f3ae3ffef
Chaos <DC3120@gmail.com>
2025-11-15 13:02:31 -0700

    Update system.onFlush.lua add graaf exemption dirty edit

--- DeadRemote-min/lua/system.onFlush.lua
+++ DeadRemote-min/lua/system.onFlush.lua
@@ -1,9 +1,14 @@
-rollInput = system.getAxisValue(0)
-pitchInput = system.getAxisValue(1)
-yawInput = -1 * system.getAxisValue(2)
+if PLAYER_ID ~= 11654 then
+  system.print('not graaf')
+  rollInput = system.getAxisValue(0)
+  pitchInput = system.getAxisValue(1)
+  yawInput = -1 * system.getAxisValue(2)
 
-Nav.axisCommandManager:setThrottleCommand(axisCommandId.longitudinal, system.getAxisValue(3))
-Nav.axisCommandManager:setThrottleCommand(axisCommandId.lateral, system.getAxisValue(4))
-Nav.axisCommandManager:setThrottleCommand(axisCommandId.vertical, system.getAxisValue(5))  
+  Nav.axisCommandManager:setThrottleCommand(axisCommandId.longitudinal, system.getAxisValue(3))
+  Nav.axisCommandManager:setThrottleCommand(axisCommandId.lateral, system.getAxisValue(4))
+  Nav.axisCommandManager:setThrottleCommand(axisCommandId.vertical, system.getAxisValue(5))
+else
+    system.print('graaf detected')
+end
 
 profile(runFlush,'runFlush')
